0665c1ee6da4b7a16b0280dff4e6aacb3966e547
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-04-15 14:08:37 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ KRON_EDGES=256
 
 for PROB in ${PROBLEMS[@]}; do
 	./src/${PROB}_base -g 17 -k 256
-  ./src/${PROB}_bf -g 17 -k 256 -t 0.1
+  ./src/${PROB}_1h -g 17 -k 256 -t 0.1
   ./src/${PROB}_bf -g 17 -k 256 -t 0.01
 done
