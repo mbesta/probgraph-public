cd61a1fbc698badfc2166642d84a07654e45bcbd
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-04-15 14:07:37 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,9 @@
-....
+PROBLEMS=(tc jp-jc jp-cn jp-ov)
+KRON_SIZE=17
+KRON_EDGES=256
+
+for PROB in ${PROBLEMS[@]}; do
+	./src/${PROB}_base -g 17 -k 256
+  ./src/${PROB}_bf -g 17 -k 256 -t 0.1
+  ./src/${PROB}_bf -g 17 -k 256 -t 0.01
+done
