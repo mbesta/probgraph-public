36c58c7496e7b7a3eab0911185a957b23b319c85
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-04-15 14:12:20 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ KRON_EDGES=256
 
 for PROB in ${PROBLEMS[@]}; do	
 	echo "*******************************"
-	echo "running ${PROB} with baseline"
+	echo "******RUNNING ${PROB} with BASELINE"
 	./src/${PROB}_base -g 17 -k 256
 	
-	echo "\n running ${PROB} with One-Hash"
+	echo "******RUNNING ${PROB} with One-Hash"
 	./src/${PROB}_1h -g 17 -k 256 -t 0.1
 	
-	echo "\n running ${PROB} with Bloom Filters"
+	echo "******RUNNING ${PROB} with Bloom Filters"
 	./src/${PROB}_bf -g 17 -k 256 -t 0.01
 done
