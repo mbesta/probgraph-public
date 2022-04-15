2a0eccd30c7644a96184b98d96a97001670f9ccf
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-04-15 14:14:49 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 PROBLEMS=(tc jp-jc jp-cn jp-ov 4c)
-KRON_SIZE=17
-KRON_EDGES=256
+KRON_SIZE=16
+KRON_EDGES=128
+OPTIONS=-g ${KRON_SIZE} -k ${KRON_EDGES} -y 0.1
 
 for PROB in ${PROBLEMS[@]}; do	
 	echo "*******************************"
 	echo "******RUNNING ${PROB} with BASELINE"
-	./src/${PROB}_base -g 17 -k 256
+	./src/${PROB}_base ${OPTIONS}
 	
 	echo "******RUNNING ${PROB} with One-Hash"
-	./src/${PROB}_1h -g 17 -k 256 -t 0.1
+	./src/${PROB}_1h ${OPTIONS} -t 0.1
 	
 	echo "******RUNNING ${PROB} with Bloom Filters"
-	./src/${PROB}_bf -g 17 -k 256 -t 0.01
+	./src/${PROB}_bf ${OPTIONS} -t 0.01
 done
