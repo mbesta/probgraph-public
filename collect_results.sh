93b7a91bef7a5607ef7a1c3f44b053e657319939
paolo.syloslabini@stud-inf.unibz.it <paolo.syloslabini@stud-inf.unibz.it>
2022-04-16 09:07:21 +0200

    changed name of csv files

--- collect_results.sh
+++ collect_results.sh
@@ -2,12 +2,12 @@ PROBLEMS=(tc jp-jc jp-cn jp-ov)
 
 for PROB in ${PROBLEMS[@]}; do
 	echo collecting kronecker results for ${PROB}
-	python src/scripts/collect_into_csv.py --input-dir kronecker_graph_results/${PROB}/ --output-name kronecker_graph_results/${PROB}.csv
+	python src/scripts/collect_into_csv.py --input-dir kronecker_graph_results/${PROB}/ --output-name kronecker_graph_results/${PROB}-kron.csv
 done
 
 for PROB in ${PROBLEMS[@]}; do
 	echo collecting real graphs results for ${PROB}
-	python src/scripts/collect_into_csv.py --input-dir real_graphs_results/${PROB}/ --output-name real_graphs_results/${PROB}.csv
+	python src/scripts/collect_into_csv.py --input-dir real_graphs_results/${PROB}/ --output-name real_graphs_results/${PROB}-real.csv
 done
 
 for PROB in ${PROBLEMS[@]}; do
