c7b2a7cd782400dd8d5811aa2301fa5476fc989e
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-04-16 09:09:48 +0200

    Update generate_images.sh

--- generate_images.sh
+++ generate_images.sh
@@ -7,10 +7,10 @@ EXPS=(tc jp-jc jp-cn jp-ov)
 for e in ${EXPS[@]}; do
 	echo images for $e
 	echo figures kroneckers
-	python src/scripts/make_comparison_images.py --input-csv kronecker_graph_results/${e}-kron.csv --image-folder kronecker_graphs_images/ --bf 0.1 --mh 0.001 --img_format pdf
+	python src/scripts/make_comparison_images.py --input-csv kronecker_graphs_results/${e}-kron.csv --image-folder kronecker_graphs_images/ --bf 0.1 --mh 0.001 --img_format pdf
 	
 	echo figures real
-	python src/scripts/make_comparison_images.py --input-csv real_graph_results/${e}-real.csv --image-folder real_graphs_images/ --kron False --img_format pdf
+	python src/scripts/make_comparison_images.py --input-csv real_graphs_results/${e}-real.csv --image-folder real_graphs_images/ --kron False --img_format pdf
 done
 
 python src/scripts/make_scaling_plots.py
